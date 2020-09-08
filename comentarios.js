6f75d28b36fb0d5cd2e2254c5ecc85c648379a9e
Hélio Giroto <h@h>
2020-09-07 21:26:25 -0300

    Adicionada o A.T. em hebraico

---
+++ comentarios.js
@@ -0,0 +1,19 @@
+let bibliaMyC = [
+['0','-'],
+
+['Dn 9:27', 'O anticristo: Dn 7:7-25; Dn 9:26-27; Dn 11:36-45; Dn 12:1-13. O resto é Antíoco Epifânio'],
+
+["Dn 7:3", "Animal diferente: Dn 7:3,7,19,23"],
+
+['Dn 7:8', 'Outro Chifre pequeno: Dn 7:8,20,24'],
+
+['Dn 8:19', 'O tempo da ira: ver tb Dn 12:9, Dn 11:36. Tempo: Dn 12:1,4,9 ... Fim: Dn 12:6,8,9,13 ...'],
+
+
+
+
+
+
+
+
+]
